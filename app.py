88a945fcbfd4ef4610ef508a8a820e18384533aa
Melek Efe <melek36200@gmail.com>
2026-06-30 12:16:05 +0300

    Dashboard skeleton and route added

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 if __name__=="__main__":
     app.run(debug=True)
